ce756fe18b5443222d7fac204c4e86ec02b18b4d
suraj-bv <surajbv5566@gmail.com>
2024-11-24 22:16:55 +0530

    All files added

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,10 @@
-# reading the no. of elements
 n = int(input('Enter the no. of elements: '))
 
-# initializing an empty list
 l = []
 
-# reading all the elements of the list
 for i in range(n):
     l.append(int(input('Enter the elements: ')))
 
-# sorting using bubble sort
 for i in range(n-1):
     for j in range(n-1-i):
         if l[j] > l[j+1]:
