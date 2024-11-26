e740118351e7f1c891e88965cfa07dbec3824adf
suraj-bv <surajbv5566@gmail.com>
2024-11-26 21:06:25 +0530

    Own palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,13 @@
 s = input('Enter the string: ')
 n = len(s)
+
 is_palindrome = True
 
-for i in range(n // 2):  # Loop through the first half of the string
-    if s[i] != s[n - i - 1]:  # Compare with the corresponding character from the end
+for i in range(n//2):
+    if s[i] != s[n - i - 1]:
         is_palindrome = False
-        break
 
 if is_palindrome:
-    print('String is a palindrome')
+    print('Given string is a palindrome')
 else:
-    print('String is not aSURA palindrome')
+    print('Given string is not a palindrome')
