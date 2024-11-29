292296a35c75643d27c6e5f4933a0862ce552706
suraj-bv <surajbv5566@gmail.com>
2024-11-29 22:45:37 +0530

    Prime nos

---
+++ prime_no.py
@@ -0,0 +1,16 @@
+n = int(input('Enter the no.: '))
+
+def prime_no(n):
+    if n < 2:
+        print("No prime numbers")
+        return
+    for num in range(2, n + 1):
+        is_prime = True
+        for divisor in range(2, int(num ** 0.5) + 1):  # Only check up to the square root of the number
+            if num % divisor == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num)
+
+print(prime_no(n))
