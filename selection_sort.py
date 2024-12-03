75afa0d6fc2006d388b42aa13f643c96785d3860
suraj-bv <surajbv5566@gmail.com>
2024-12-03 23:26:55 +0530

    All files added

--- selection_sort.py
+++ selection_sort.py
@@ -1,10 +1,7 @@
-# reading the no. of elements
 n = int(input('Enter the no. of elements: '))
 
-# initializing an empty list
 l = []
 
-# reading all the elements of the list
 for i in range(n):
     l.append(int(input('Enter the elements: ')))
 
