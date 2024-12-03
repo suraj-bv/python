92e01d1db370590c5209b23f65c7f87a5ba63542
suraj-bv <surajbv5566@gmail.com>
2024-12-03 23:24:49 +0530

    All files added

--- largest_no.py
+++ largest_no.py
@@ -2,11 +2,11 @@ n = int(input('Enter the no. of items in the list: '))
 
 l = [int(input('Enter the item of the list: ')) for i in range(n)]
 
-def largest_no(lis):
-    largest = lis[0]
+def largest_no(li):
+    largest = li[0]
     for i in range(1, n):
-        if lis[i] > largest:
-            largest = lis[i]
+        if li[i] > largest:
+            largest = li[i]
     return largest
 
 print(f'Largest no in the list is :{largest_no(l)}')
