8c6f81155854517717b04974c2ddc80c32dbedd5
suraj-bv <surajbv5566@gmail.com>
2024-12-03 23:27:35 +0530

    All files added

--- frequent_element.py
+++ frequent_element.py
@@ -1,10 +1,7 @@
-#Develop a function that finds the most frequent element in a list.
-
 def most_frequent_element(lst):
-    if not lst:  # Check if the list is empty
+    if not lst:
         return None
 
-    # Create a dictionary to store frequencies
     frequency = {}
     for item in lst:
         if item in frequency:
@@ -12,7 +9,6 @@ def most_frequent_element(lst):
         else:
             frequency[item] = 1
 
-    # Find the element with the maximum frequency
     max_count = -1
     most_frequent = None
     for item, count in frequency.items():
@@ -22,6 +18,5 @@ def most_frequent_element(lst):
 
     return most_frequent
 
-# Example usage:
 my_list = [1, 2, 2, 3, 4, 2, 5, 3]
-print(most_frequent_element(my_list))  # Output: 2
+print(most_frequent_element(my_list))
