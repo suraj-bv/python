a68f98b1f708cc4e2509a4d44f682e76c892cc71
suraj-bv <surajbv5566@gmail.com>
2025-01-06 13:11:29 +0530

    Reverse a list

---
+++ reverse_list.py
@@ -0,0 +1,11 @@
+n = input('Enter the no. of items to insert: ')
+
+items = []
+
+for i in range(n):
+    item = input(f'Enter item {i+1}: ')
+    items.append(item)
+
+def reverse(items):
+    while(len(items) >-1):
+        items[]
