c0b1d5c3cfc43cb7da943b8deff62748b1faa6b1
suraj-bv <surajbv5566@gmail.com>
2025-07-09 20:01:11 +0530

    Roman no. to int py code

---
+++ roman-2-int.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev = 0
+        
+        for ch in reversed(s):
+            curr = roman[ch]
+            if curr < prev:
+                total -= curr
+            else:
+                total += curr
+            prev = curr
+            
+        return total
