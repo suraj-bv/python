9c3f243585695029767d8f406b5fbb50b46e2114
suraj-bv <surajbv5566@gmail.com>
2025-07-15 22:27:08 +0530

    Updated swap.py added 3 method to swap number arithmetic, XOR & by Python

--- swap.py
+++ swap.py
@@ -1,10 +1,30 @@
-def swap(a, b):
+def swap_by_arithmetic_op(a, b):
     a = a - b
     b = a + b
     a = a - b
     return a, b
 
 a, b = 20, 30
-print(f'Before swapping: a = {a}, b = {b}')
-a, b = swap(a, b)
-print(f'After swapping: a = {a}, b = {b}')
+print(f'Before swapping using arithmetic operations: a = {a}, b = {b}')
+a, b = swap_by_arithmetic_op(a, b)
+print(f'After swapping using arithmetic operations: a = {a}, b = {b}')
+
+def swap_by_xor(a, b):
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    return a, b
+
+a, b = 20, 30
+print(f'Before swapping using XOR: a = {a}, b = {b}')
+a, b = swap_by_xor(a, b)
+print(f'After swapping using XOR: a = {a}, b = {b}')
+
+def swap_by_py(a, b):
+    a, b = b, a
+    return a, b
+
+a, b = 20, 30
+print(f'Before swapping using Python: a = {a}, b = {b}')
+a, b = swap_by_py(a, b)
+print(f'After swapping using Python: a = {a}, b = {b}')
