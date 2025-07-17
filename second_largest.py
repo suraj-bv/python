01d8cccc53d86ba54046f62c6f8e61c5abdd0548
suraj-bv <surajbv5566@gmail.com>
2025-07-17 21:56:23 +0530

    Understood logic of second largest

--- second_largest.py
+++ second_largest.py
@@ -1,20 +1,27 @@
-l = [1,5,15,2,35,7,8,11,9,66]
+l = [1,5,15,2,35,7,8,11,9,66,4]
 
-n = len(l)
+# n = len(l)
 
-def swap(a, b):
-    l[a] = l[a] + l[b]
-    l[b] = l[a] - l[b]
-    l[a] = l[a] - l[b]
+# def swap(a, b):
+#     l[a] = l[a] + l[b]
+#     l[b] = l[a] - l[b]
+#     l[a] = l[a] - l[b]
 
-for i in range(n-1):
-    min = i
-    for j in range(i+1, n):
-        if l[min] > l[j]:
-            min = j
-    if min != i:
-        swap(min, i)
+# for i in range(n-1):
+#     min = i
+#     for j in range(i+1, n):
+#         if l[min] > l[j]:
+#             min = j
+#     if min != i:
+#         swap(min, i)
         
-print(l)
+# print(l)
 
-print(f'Second largest element is {l[-2]}')
+# print(f'Second largest element is {l[-2]}')
+
+for i in range(len(l)-1):
+    max = l[i]
+    if max < l[i+1]:
+        max = l[i+1]
+    
+print(max)
