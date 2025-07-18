ef34f591469ccad875b2ced1bc96b9f6e523e282
suraj-bv <surajbv5566@gmail.com>
2025-07-18 22:10:55 +0530

    Added py code to move all zero to end of list another method

--- push_zero_to_end.py
+++ push_zero_to_end.py
@@ -1,15 +1,35 @@
 #User function Template for python3
 
 class Solution:
-	def pushZerosToEnd(self,arr):
-	    new_arr = []
-	    count = 0
-    	for i in range(len(arr)):
-    	    if arr[i] != 0:
-    	        new_arr.append(arr[i])
-    	    else:
-    	        count+=1
-    	
-    	new_arr.extend([0] * count)
-    	
-    	return new_arr
+	def pushZerosToEnd(self, arr):
+		new_arr = []
+		count = 0
+		for i in range(len(arr)):
+			if arr[i] != 0:
+				new_arr.append(arr[i])
+			else:
+				count += 1
+
+		new_arr.extend([0] * count)
+
+		return new_arr
+	
+
+
+# #User function Template for python3
+
+# class Solution:
+# 	def pushZerosToEnd(self,arr):
+# 	    idx = 0
+# 	    n = len(arr)
+	    
+# 	    for i in range(n):
+# 	        if arr[i] != 0:
+# 	            arr[idx] = arr[i]
+# 	            idx += 1
+	            
+# 	    while idx < n:
+# 	        arr[idx] = 0
+# 	        idx += 1
+	         
+# 	    return arr
