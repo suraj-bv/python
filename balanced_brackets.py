cf250229363adfea21c957bcdb6764d8323d5458
suraj-bv <surajbv5566@gmail.com>
2025-07-20 09:17:08 +0530

    Py code to balance the brackets

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -15,3 +15,21 @@ test_strings = ["()", "(())", "(()[])", "{[()]}", "({[()]})", "(", ")", "({[}])"
 
 for s in test_strings:
     print(f"{s}: {'Balanced' if is_balanced(s) else 'Not Balanced'}")
+
+
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         mapping = {')' : '(', '}':'{', ']': '['}
+
+#         for char in s:
+#             if char in ")}]":
+#                 if not stack or stack[-1] != mapping[char]:
+#                     return False
+#                 stack.pop()
+#             else:
+#                 stack.append(char)
+            
+#         return not stack
+# another solution using stack
