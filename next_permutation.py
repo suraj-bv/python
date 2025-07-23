d8ea80d14550a9ef1aa41163bdc993b215954bd3
suraj-bv <surajbv5566@gmail.com>
2025-07-23 22:20:14 +0530

    Next permutation

---
+++ next_permutation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextPermutation(self, arr):
+        n = len(arr)
+        i = n - 2
+        
+        while i >= 0 and arr[i] >=arr[i+1]:
+            i -= 1
+            
+        if i>=0:
+            j = n-1
+            while arr[j] <= arr[i]:
+                j -= 1
+                
+            arr[i], arr[j] = arr[j], arr[i]
+        
+        arr[i + 1:] = reversed(arr[i + 1:])
+        
+        return arr
+    
+# this code finds the next lexicographical permutation of a list of integers
+# it modifies the list in place and returns it
