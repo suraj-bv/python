da48322317763f9c9e47c309ff70bffd84af0292
suraj-bv <surajbv5566@gmail.com>
2025-08-02 14:32:35 +0530

    reverse_list added

--- reverse_list.py
+++ reverse_list.py
@@ -1,11 +1,26 @@
-n = input('Enter the no. of items to insert: ')
+# n = input('Enter the no. of items to insert: ')
 
-items = []
+# items = []
 
-for i in range(n):
-    item = input(f'Enter item {i+1}: ')
-    items.append(item)
+# for i in range(n):
+#     item = input(f'Enter item {i+1}: ')
+#     items.append(item)
 
-def reverse(items):
-    while(len(items) >-1):
-        items[]
+# def reverse(items):
+#     while(len(items) >-1):
+#         items[]
+
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        for i in range(n//2):
+            s[i], s[n-1-i] = s[n-1-i], s[i]
+
+
+sol = Solution()
+sol.reverseString(["h", "e", "l", "l", "o"])
