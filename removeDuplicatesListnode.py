5343b51ab20e3c412a467ba8bca62cf5b3bdfb3f
suraj-bv <surajbv5566@gmail.com>
2025-08-03 22:14:47 +0530

    Updated removeDuplicates

--- removeDuplicatesListnode.py
+++ removeDuplicatesListnode.py
@@ -6,6 +6,7 @@ from typing import ListNode
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
