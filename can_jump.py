4d6a065abef71347ec2c113477e275bdb70af7f8
suraj-bv <surajbv5566@gmail.com>
2025-08-08 22:06:23 +0530

    Added can_jump.py

---
+++ can_jump.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxIdx = 0
+        for idx in range(len(nums)):
+            if idx > maxIdx:
+                return False
+            elif maxIdx < nums[idx] + idx:
+                maxIdx = nums[idx] + idx
+
+        return True
