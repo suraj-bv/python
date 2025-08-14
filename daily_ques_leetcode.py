ccbfe7d02f52ac169fb146b609cb7ddb1ae525d5
suraj-bv <surajbv5566@gmail.com>
2025-08-15 00:00:16 +0530

    Solved daily leetcode Day 2

--- daily_ques_leetcode.py
+++ daily_ques_leetcode.py
@@ -8,4 +8,14 @@ class Solution:
 
         return n == 1
     
-
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        max_good = ""
+        
+        for i in range(len(num) - 2):
+            sub = num[i:i+3]
+            if sub[0] == sub[1] == sub[2]:
+                if sub > max_good:
+                    max_good = sub
+                    
+        return max_good
