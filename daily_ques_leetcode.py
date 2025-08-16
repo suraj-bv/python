1642af2bd3fa6923108283434b9db12a79d3b095
suraj-bv <surajbv5566@gmail.com>
2025-08-16 16:52:49 +0530

    Daily question streak broken started Day 1 omce again

--- daily_ques_leetcode.py
+++ daily_ques_leetcode.py
@@ -19,3 +19,14 @@ class Solution:
                     max_good = sub
                     
         return max_good
+
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num_str = list(str(num))
+        for i in range(len(num_str)):
+            if num_str[i] == '6':
+                num_str[i] = '9'
+                break
+        return int("".join(num_str))
+
