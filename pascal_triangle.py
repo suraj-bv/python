f5ddb2003f1801dd20ba022f6d8a2b981521c1f4
suraj-bv <surajbv5566@gmail.com>
2025-08-21 23:45:05 +0530

    Added pascal triangle code for a particular rowIndex

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -11,3 +11,10 @@ class Solution:
             triangle.append(new_row)
 
         return triangle
+    
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(rowIndex):
+            row = [x + y for x, y in zip([0] + row, row + [0])]
+        return row
