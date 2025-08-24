f9af39f15d30145b3b7272b5fc30abf3bccfbd3a
suraj-bv <surajbv5566@gmail.com>
2025-08-24 21:57:11 +0530

    updated fibonnaci_series.py

--- fibonnaci_series.py
+++ fibonnaci_series.py
@@ -14,3 +14,13 @@ def fibonacci(num):
         return fibonnaci_series
 
 print(f'Number of fibonacci {fibonacci(num)}')
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)
