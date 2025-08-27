11a04d26e97a4027bca0d8744106046dee3e476d
suraj-bv <surajbv5566@gmail.com>
2025-08-27 19:43:04 +0530

    Added py code to compute the no. of days in a year

---
+++ days_in_year.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = map(int, date.split("-"))
+
+        days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            days_in_month[1] = 29
+
+        return sum(days_in_month[:month - 1]) + day
