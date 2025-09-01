1543c3573a74820c13de1232190afe477dc7026e
suraj-bv <surajbv5566@gmail.com>
2025-09-01 23:44:36 +0530

    Added binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -27,3 +27,23 @@ for i in range(n):
 key = int(input('Enter the key element :'))
 
 search(l,key)
+
+
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+
+        return -1
