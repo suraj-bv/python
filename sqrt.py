2c2c3e4c7f764298a7d449023b5571ee71c7b0ea
suraj-bv <surajbv5566@gmail.com>
2025-09-08 22:25:11 +0530

    Added sorted squares code to sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -4,3 +4,11 @@ import math
 class Solution:
     def mySqrt(self, x: int) -> int:
         return int(math.sqrt(x))
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] *= nums[i]
+        
+        nums.sort()
+        return nums
