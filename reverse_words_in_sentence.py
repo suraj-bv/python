c180072d8b5933bb9465fd36f1db5a093f9ef01c
suraj-bv <surajbv5566@gmail.com>
2025-09-10 23:20:44 +0530

    Sloved leetcode problem to reverse the letters in a word in a sentence

--- reverse_words_in_sentence.py
+++ reverse_words_in_sentence.py
@@ -4,3 +4,13 @@ class Solution:
         words.reverse()
 
         return " ".join(words)
+    
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s_list = s.split()
+        
+        for i in range(len(s_list)):
+            s_list[i] = s_list[i][::-1]
+
+        return ' '.join(s_list)
