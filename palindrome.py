876d139e1f4c33e768f9284f26ab2f5677254be6
suraj-bv <surajbv5566@gmail.com>
2025-09-17 21:26:32 +0530

    Updated palindrome.py to get the length of longest valid palindrome

--- palindrome.py
+++ palindrome.py
@@ -25,3 +25,22 @@ class Solution:
                 return False
 
         return True
+
+
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freq = Counter(s)
+        length = 0
+        odd_found = False
+
+        for count in freq.values():
+            length += (count // 2) * 2
+            if count % 2 == 1:
+                odd_found = True
+
+        if odd_found:
+            length += 1
+
+        return length
