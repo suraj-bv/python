be957e245613fa488a579f5bf17b9a34012ca150
suraj-bv <surajbv5566@gmail.com>
2025-09-17 21:16:48 +0530

    Updated word_pattern.py to add the same type of function to check if two stringss are isomorphic or not

--- word_pattern.py
+++ word_pattern.py
@@ -18,3 +18,23 @@ class Solution:
                     return False
         
         return True
+
+
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping = {}
+        used = set()
+
+        for ch1, ch2 in zip(s, t):
+            if ch1 not in mapping:
+                if ch2 in used:
+                    return False
+                mapping[ch1] = ch2
+                used.add(ch2)
+            else:
+                if mapping[ch1] != ch2:
+                    return False
+
+        return True
