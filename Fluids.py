03f4956787023d5511687d9108e649a5a9804b21
suraj-bv <surajbv5566@gmail.com>
2025-09-23 00:00:09 +0530

    All files added

---
+++ Fluids.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        max_area = 0
+
+        while left < right:
+            # calculate area
+            area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, area)
+
+            # move pointer
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
