e238ccfcbeffeb257b3eb30b2616970163004b0a
suraj-bv <surajbv5566@gmail.com>
2025-10-06 23:21:19 +0530

    Solved leetcode 1614 to get the max depth of the parantheses

---
+++ max_depth_parantheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth = 0
+        max_depth = 0
+
+        for char in s:
+            if char == '(':
+                depth += 1
+                max_depth = max(depth, max_depth)
+            elif char == ')':
+                depth -= 1
+
+        return max_depth
