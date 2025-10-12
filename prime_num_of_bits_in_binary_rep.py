c366ddc8970929699a7f812c45990ee1521f6728
suraj-bv <surajbv5566@gmail.com>
2025-10-12 22:38:40 +0530

    Solved leetcode 762 to return the number of times the prime num of ones is obtained while iterating through nums

---
+++ prime_num_of_bits_in_binary_rep.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        primes = [2, 3, 5, 7, 11, 13, 17, 19]
+        count = 0
+        
+        for i in range(left, right+1):
+            bits = bin(i).count('1')
+            if bits in primes:
+                count += 1
+
+        return count
