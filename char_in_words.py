f2020a1593044c123444f90a3bc76b8c0a7d8c11
suraj-bv <surajbv5566@gmail.com>
2025-10-19 22:54:07 +0530

    Solved leetcode 2842 which returns the indices of the words that conatin x a char in the strings

---
+++ char_in_words.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res = []
+
+        for i, word in enumerate(words):
+            if x in word:
+                res.append(i)
+
+        return res
