d78b203be8c3db3c0342162902cfc85974102f01
suraj-bv <surajbv5566@gmail.com>
2025-10-26 23:15:00 +0530

    Solved leetcode 2894

---
+++ num_divisible.py
@@ -0,0 +1,12 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        div_sum = 0
+        non_div_sum = 0
+        
+        for i in range(1, n + 1):
+            if i % m == 0:
+                div_sum += i
+            else:
+                non_div_sum += i
+                
+        return non_div_sum - div_sum
