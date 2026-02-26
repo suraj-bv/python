1a0ee11bbbae63605855486648bff03d23756ed3
suraj-bv <surajbv5566@gmail.com>
2026-02-26 23:54:10 +0530

    Solved leetcode 2053

---
+++ Kth_Distinct_String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        res = []
+        
+        for i in range(len(arr)):
+            if arr[i] not in (arr[:i] + arr[i+1:]):
+                res.append(arr[i])
+
+        return res[k-1] if k <= len(res) else ""
