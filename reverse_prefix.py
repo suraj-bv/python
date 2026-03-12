d9618db4c923cf198f3de450974bdbc45eb2483f
suraj-bv <surajbv5566@gmail.com>
2026-03-13 00:09:49 +0530

    SOlved leetcode 2089

--- reverse_prefix.py
+++ reverse_prefix.py
@@ -9,3 +9,12 @@ class Solution:
 
         rev_prefix = prefix[::-1]
         return rev_prefix + word
+
+
+class Solution:
+    def reversePrefix(self, s: str, k: int) -> str:
+        rev = s[:k]
+        rev = rev[::-1]
+        rev += s[k:]
+
+        return rev
